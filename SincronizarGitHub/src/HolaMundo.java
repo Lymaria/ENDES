98f20d432ac7c4cdcd3ad2c385ea5f87769f8526
DAM1 <DAM1@PC>
2025-02-19 09:08:18 +0100

    19/02/2025 - V0.0.0 - LidiaMG - Importado desde git, a un eclipse nuevo

--- SincronizarGitHub/src/HolaMundo.java
+++ SincronizarGitHub/src/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
 		System.out.println("Hola, mundo!: Nueva version");
 		//Comentario de in compañero
 		System.out.println("Hemos importado el proyecto desde github");
+		System.out.println("Hecho de verdad, descargando un eclipse nuevo");
 
 	}
 
